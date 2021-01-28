4b46648f2dd25afab81171475ada02e0b2e754e4
OrkunSA <c.orkun.s@gmail.com>
2021-01-27 18:06:18 -0600

    create Dashboard Home Login Navbar and Signup componenets

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { logout } from "../redux/actions/authActions";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <div>
+        <Link to="/">Home</Link>
+        {this.props.loggedIn ? (
+          <>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="#" onClick={() => this.props.logout(this.props.history)}>
+              Logout
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to="/signup">Signup</Link>
+            <Link to="/login">Login</Link>
+          </>
+        )}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: state.auth.loggedIn,
+  };
+};
+
+export default withRouter(connect(mapStateToProps, { logout })(Navbar));
