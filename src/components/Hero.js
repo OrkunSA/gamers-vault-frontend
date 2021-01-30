34381bde3ec5d0e1e9362172e93c418f17a1fc46
OrkunSA <c.orkun.s@gmail.com>
2021-01-30 13:14:23 -0600

    fix button url and styling in hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../App.css";
 import { Button } from "./Button";
 import "./Hero.css";
+import { Link } from "react-router-dom";
 
 function Hero() {
   return (
@@ -11,11 +12,14 @@ function Hero() {
       <p>Get Ready!</p>
       <div className="hero-btns">
         <Button
-          className="btns"
+          className="btn"
           buttonStyle="btn--primary"
           buttonSize="btn--large"
         >
-          BROWSE GAMES <i className="fas fa-search"></i>
+          <Link to="/browse" className="btn" color={{ color: "#242424" }}>
+            BROWSE GAMES
+          </Link>
+          <i className="fas fa-search"></i>
         </Button>
       </div>
     </div>
