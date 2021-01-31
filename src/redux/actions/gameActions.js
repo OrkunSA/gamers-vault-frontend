45a3c17827c41af3e3beede13be038a0769eefc5
OrkunSA <c.orkun.s@gmail.com>
2021-01-30 20:54:51 -0600

    refactoring gameActions

--- src/redux/actions/gameActions.js
+++ src/redux/actions/gameActions.js
@@ -1,26 +1,18 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
-const API_URL = `https://api.rawg.io/api/games?key=${API_KEY}`;
-export const getGames = () => {
-  return (dispatch) => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((data) => dispatch({ type: "SET_GAMES", payload: data }));
-  };
-};
+const NEW_RELEASE_URL = `https://api.rawg.io/api/games?dates=2021-01-01,2021-01-29&platforms=18,1,7?key=${API_KEY}`;
 
-export const addGamesToCollection = (data) => {
+function getGames(games) {
+  return { type: "SET_GAMES", payload: games.results };
+}
+
+function gettingGames() {
   return (dispatch) => {
-    fetch(`http://localhost:3001/games`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-      body: JSON.stringify({ game: data }),
-    })
-      .then((res) => res.json())
-      .then((game) =>
-        dispatch({ type: "ADD_GAME_TO_COLLECTION", payload: game })
-      );
+    fetch(NEW_RELEASE_URL)
+      .then((resp) => resp.json())
+      .then((result) => {
+        dispatch(getGames(result));
+      });
   };
-};
+}
+
+export { gettingGames };
