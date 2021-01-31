1479b5de6d2ae74482528d2bc3696e0013c6a70c
OrkunSA <c.orkun.s@gmail.com>
2021-01-30 20:53:39 -0600

    combine game reducer

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import authReducer from "./reducers/authReducer";
 import thunk from "redux-thunk";
 import gameReducer from "./reducers/gameReducer";
+
 import logger from "redux-logger";
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  games: gameReducer,
+  released: gameReducer,
+  // search: searchReducer,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
