221b236d6d6728072a2c6b23ecf8380bfd2f4a96
OrkunSA <c.orkun.s@gmail.com>
2021-02-01 19:04:13 -0600

    create collection card component

---
+++ src/components/CollectionCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Collection.css";
+class CollectionCard extends React.Component {
+  render() {
+    let { name, picture } = this.props.gameObj;
+
+    const cardStyle = {
+      background: `url(${picture})`,
+      backgroundSize: "100% 100%",
+      backgroundPosition: "center center",
+      width: "auto",
+      height: "16vw",
+    };
+    return (
+      <>
+        <div>
+          <div className="search-game-card animated fadeIn">
+            <Link to={`/game/${this.props.gameObj.id}`}>
+              <div style={cardStyle} className="search-game-card-image"></div>
+            </Link>
+            <Link
+              className="search-game-text"
+              to={`/game/${this.props.gameObj.id}`}
+            >
+              {name}
+            </Link>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default CollectionCard;
