ed01870345b5b86b8616b0fc9192e7322a2f6807
OrkunSA <c.orkun.s@gmail.com>
2021-02-01 19:08:28 -0600

    fetch games from backend and render collectioncards

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,15 +1,49 @@
 import React from "react";
+import CollectionCard from "./CollectionCard";
 import { connect } from "react-redux";
+import { gettingCollection } from "../redux/actions/gameActions";
 
-const Collection = ({ user }) => {
-  console.log(user);
-  return <h1>Welcome {user.email}</h1>;
-};
+class Collection extends React.Component {
+  componentDidMount() {
+    this.props.gettingCollection();
+  }
+
+  render() {
+    return (
+      <div className="results-game-section">
+        {this.props.gameCollection.map((game) => {
+          return <CollectionCard key={game.id} gameObj={game} />;
+        })}
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
-    user: state.auth.currentUser,
+    gameCollection: state.collection,
   };
 };
 
-export default connect(mapStateToProps)(Collection);
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     gettingCollection: () => {
+//       dispatch(gettingCollection);
+//     },
+//   };
+// };
+
+export default connect(mapStateToProps, { gettingCollection })(Collection);
+
+// const Collection = ({ user }) => {
+//   console.log(user);
+//   return <h1>Welcome {user.email}</h1>;
+// };
+
+// const mapStateToProps = (state) => {
+//   return {
+//     user: state.auth.currentUser,
+//   };
+// };
+
+// export default connect(mapStateToProps)(Collection);
