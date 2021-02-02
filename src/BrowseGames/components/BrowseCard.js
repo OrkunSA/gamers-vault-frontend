67b858776af82c1b3ad2070fb9bb4b3bfca9ad8f
OrkunSA <c.orkun.s@gmail.com>
2021-02-01 19:10:41 -0600

    add games to the backend collection and render each gamecard

--- src/BrowseGames/components/BrowseCard.js
+++ src/BrowseGames/components/BrowseCard.js
@@ -1,7 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import { connect } from "react-redux";
 
 class BrowseCard extends React.Component {
+  handleOnClick = (event) => {
+    fetch(`http://localhost:3001/games/${event.id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        name: event.name,
+        picture: event.background_image,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        switch (data.message) {
+          case "Already added!":
+            Swal.fire({
+              title: "Already added!",
+              text: "Browse more games!",
+              icon: "error",
+              confirmButtonText: "Back",
+            });
+            break;
+          default:
+            Swal.fire({
+              title: "Added!",
+              text: "Game has been added",
+              icon: "success",
+              confirmButtonText: "Cool",
+            });
+        }
+      });
+  };
+
   render() {
     let { name, background_image } = this.props.gameObj;
 
@@ -14,19 +51,33 @@ class BrowseCard extends React.Component {
     };
 
     return (
-      <div className="search-game-card animated fadeIn">
-        <Link to={`/game/${this.props.gameObj.id}`}>
-          <div style={cardStyle} className="search-game-card-image"></div>
-        </Link>
-        <Link
-          className="search-game-text"
-          to={`/game/${this.props.gameObj.id}`}
-        >
-          {name}
-        </Link>
-      </div>
+      <>
+        <div>
+          <div className="search-game-card animated fadeIn">
+            <Link to={`/game/${this.props.gameObj.id}`}>
+              <div style={cardStyle} className="search-game-card-image"></div>
+            </Link>
+            <Link
+              className="search-game-text"
+              to={`/game/${this.props.gameObj.id}`}
+            >
+              {name}
+            </Link>
+          </div>
+          {this.props.loggedIn && (
+            <button onClick={() => this.handleOnClick(this.props.gameObj)}>
+              Add to collection
+            </button>
+          )}
+        </div>
+      </>
     );
   }
 }
 
-export default BrowseCard;
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: state.auth.loggedIn,
+  };
+};
+export default connect(mapStateToProps)(BrowseCard);
