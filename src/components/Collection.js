43f2f234d124e9247fc66d8c8059175e77315743
OrkunSA <c.orkun.s@gmail.com>
2021-02-04 21:04:45 -0600

    deleting from collection is working now, was passing wrong id

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,9 +1,6 @@
 import React from "react";
 import CollectionCard from "./CollectionCard";
-import { connect } from "react-redux";
-import { gettingCollection } from "../redux/actions/gameActions";
 import Swal from "sweetalert2";
-
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import "./Collection.css";
@@ -26,12 +23,7 @@ class Collection extends React.Component {
       });
   }
 
-  // console.log(fetchedGames);
-  // this.setState({ games: fetchedGames });
-
   deleteFromCollection = (id) => {
-    let currentGames = [...this.state.games];
-
     fetch(`http://localhost:3001/user/collections/${id}`, {
       method: "DELETE",
       headers: {
@@ -41,13 +33,13 @@ class Collection extends React.Component {
     })
       .then((res) => res.json(res))
       .then((data) => {
+        let updatedGames = this.state.games.filter((game) => game.id !== id);
+        this.setState({ games: updatedGames });
         Swal.fire({
           title: "Deleted!",
           text: "Game has been deleted",
           icon: "success",
         });
-        let updatedGames = currentGames.filter((game) => game.id !== data.id);
-        this.setState({ games: updatedGames });
       });
   };
 
@@ -96,11 +88,4 @@ class Collection extends React.Component {
   }
 }
 
-// const mapStateToProps = (state) => {
-//   return {
-//     gameCollection: state.collection,
-//   };
-// };
-
 export default Collection;
-// export default connect(mapStateToProps, { gettingCollection })(Collection);
